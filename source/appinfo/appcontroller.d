d9607a30c4966d0952386c0a296b125778c6a9f9
John Simiyu <jonnyeinstein@gmail.com>
2022-06-12 12:29:03 +0300

    mingle app controller

--- source/appinfo/appcontroller.d
+++ source/appinfo/appcontroller.d
@@ -1,2 +1,20 @@
 module appinfo.appcontroller;
 
+import vibe.http.common : HTTPMethod;
+import vibe.web.rest;
+import vibe.vibe;
+import vibe.web.auth;
+import utils.restcontroller : RestController;
+import utils.loggeduser;
+
+@path("/")
+class AppController : RestController
+{
+    void index(HTTPServerRequest req, HTTPServerResponse res)
+    {
+        string msg = `{"message":"welcome to Ming.le API v1.0.0"}`;
+        JSONValue json = parseJSON(msg);
+        res.writeJsonBody(json);
+    }
+
+}
